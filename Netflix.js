b0c1818b8435e59b6429fcecbafcd8a86bf021bb
Cityresidents <106128047+Cityresidents@users.noreply.github.com>
2022-08-15 15:21:21 +0800

    Update Netflix.js

--- Netflix.js
+++ Netflix.js
@@ -31,7 +31,7 @@ const FILM_ID = 81215567
 const AREA_TEST_FILM_ID = 80018499
 const DEFAULT_OPTIONS = {
   title: '𝗦𝘂𝗽𝗲𝗿 𝘀𝘁𝗿𝗲𝗮𝗺𝗶𝗻𝗴 𝗡𝗲𝘁𝗳𝗹𝗶𝘅 (解锁检测)',
-  fullContent: '完整解锁𝗡𝗲𝘁𝗳𝗹𝗶𝘅 ➠ #REGION_FLAG# #REGION_NAME#',
+  fullContent: '完整解锁 𝗦𝘁𝗿𝗲𝗮𝗺𝗶𝗻𝗴 𝗡𝗲𝘁𝗳𝗹𝗶𝘅 ➠ #REGION_FLAG# #REGION_NAME#',
   fullIcon: 'suit.heart.fill',
   fullIconColor: '#FF69B4',
   fullStyle: 'good',
